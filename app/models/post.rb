3f10fd73d06d9c43a758dc65c67787611d9809a5
Arsany Ashraf <arsanyarsany4800@gmail.com>
2026-06-14 16:03:15 +0300

    Add association and realtions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  has_many :post_editors, dependent: :destroy
+  has_many :editors, through: :post_editors, source: :editor
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :created_posts, class_name: "Post", foreign_key: "creator_id", dependent: :destroy
+  has_many :post_editors, foreign_key: "editor_id", dependent: :destroy
+  has_many :edited_posts, through: :post_editors, source: :post
 end
