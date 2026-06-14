aaafed1d3c81d4ae1d8f26f0484b7b9db801377c
Arsany Ashraf <arsanyarsany4800@gmail.com>
2026-06-14 14:30:43 +0300

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :DOB, :email, :phone_number, :address
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+    message: "must be a valid email addresss"
+  }
 end
