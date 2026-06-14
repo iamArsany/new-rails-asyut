a3e4480160c02a3bbeb9aeb889b2bbba491ee2db
Arsany Ashraf <arsanyarsany4800@gmail.com>
2026-06-14 14:41:28 +0300

    Add address to User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :name, :DOB, :email, :phone_number ])
+      params.expect(user: [ :name, :DOB, :email, :phone_number, :address ])
     end
 end
